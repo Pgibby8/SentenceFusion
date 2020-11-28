1b740841333c398745b6d17b656f7373f85d6d71
Paul Gibby <pgibby8@hotmail.com>
2020-11-27 19:43:01 -0500

    fixing import typo

--- SentenceFusion/sentence_fusion.py
+++ SentenceFusion/sentence_fusion.py
@@ -1,8 +1,6 @@
 import torch
 import numpy as np
 from transformers import (
-    EncoderDecoderModel,
-    BertTokenizer
     LogitsProcessorList,
     MinLengthLogitsProcessor,
     TopKLogitsWarper,
